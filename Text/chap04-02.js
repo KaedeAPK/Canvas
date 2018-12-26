61c4d21994af8469ca9c4fde81670c2caac6dcdf
kaedeAPK <curiosity.yg57@gmail.com>
2018-12-27 03:57:27 +0900

    add pixel plane girdge

--- Text/chap04-02.js
+++ Text/chap04-02.js
@@ -8,23 +8,28 @@ const midY = cvs.height /2;
 const plane = new Image();
 plane.src = './firstPlane.png';
 
-// var flag = false;
-// plane.onload = function() {
-//     flag = true;
-// }               
-plane.addEventListener('load',loaded, false);
+var flag = false;
+plane.onload = function() {
+    flag = true;
+}               
 
-function loaded() {
-    render();
-}
+let planeX = midX; //render()内だとなぜか動かない
+let dx = 5;
+// plane.addEventListener('load',loaded, false);
+// function loaded() {
+//     render();
+// }
 
 function render() {
-    // if (flag)
-    let planeX = midX;
-        c.clearRect(0,0,endX,endY);
+    c.clearRect(0,0,endX,endY);
+    if (flag)        
         c.drawImage(plane, planeX,midY,64,64);
-        c.drawImage(plane, 200,200);
-        console.log(planeX);
-        planeX ++;
+        // c.drawImage(plane, 200,200);
         requestAnimationFrame(render);
+        planeX += dx;
+        if (planeX < endX)
+            planeX *= -1;
+        else
+            planeX -= dx;
+        console.log(planeX);
  } render();
