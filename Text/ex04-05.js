ee91cd03b9c778c87db6eefe7947d380da82d27a
kaedeAPK <curiosity.yg57@gmail.com>
2018-12-31 18:59:13 +0900

    understand %8 frame adimin

---
+++ Text/ex04-05.js
@@ -0,0 +1,69 @@
+// by chap04.html
+const cvs = document.querySelector('canvas');
+const c = cvs.getContext('2d');
+const endX = cvs.width = window.innerWidth;
+const endY = cvs.height= window.innerHeight;
+const midX = cvs.width /2;
+const midY = cvs.height /2;
+
+const px = 32; const size = 64;
+
+var imgW;
+
+const tile = new Image();
+tile.src = './pic/tanks_sheet.png';
+
+var flag = false;
+tile.onload = function() {
+    flag = true;
+    imgW = tile.naturalWidth;
+};       
+
+var counter = 1;
+
+var tileIndex = 1; //0-7
+
+
+
+
+// c.drawImage(var of image, sx, sy, sw, sh, 
+//  x, y, width , height)
+
+function render() {
+
+    // 1 % 8 = 1... x32
+    // 2 % 8: 2 x32
+    // 9 % 8: 1 x 32
+    
+    var sx = (tileIndex+1)%8*px;
+  
+
+
+    // tileNum[0],つまり1 を8で割ったものの整数x32
+    //           sx,sy: 32 0
+// ex04-05.js:41 sx,sy: 64 0
+// ex04-05.js:41 sx,sy: 96 0
+// ex04-05.js:41 sx,sy: 128 0
+// ex04-05.js:41 sx,sy: 160 0
+// ex04-05.js:41 sx,sy: 192 0
+// ex04-05.js:41 sx,sy: 224 0
+// ex04-05.js:41 sx,sy: 0 32
+// ex04-05.js:41 sx,sy: 32 0
+
+    var sy = Math.floor(tileNum[tileIndex]/8)*px;
+    console.log('sx,sy:',sx,sy);
+
+    c.clearRect(0,0,endX,endY);
+        if (flag)
+        c.drawImage(tile, sx,sy,px,px,
+            midX,100+(size),size,size);
+                
+        tileIndex++;
+        if (tileNum.length == tileIndex)
+            tileIndex = 0;// 0-7 -> 1-8
+            
+        requestAnimationFrame(render); // 1000/30fps 
+} render();
+
+
+     
---
+++ Text/ex04-5byJP.js
@@ -0,0 +1,53 @@
+// by chap04.html
+const cvs = document.querySelector('canvas');
+const c = cvs.getContext('2d');
+const endX = cvs.width = window.innerWidth;
+const endY = cvs.height= window.innerHeight;
+const midX = cvs.width /2;
+const midY = cvs.height /2;
+
+const px = 32; const size = 64;
+
+var imgW;
+
+const tile = new Image();
+tile.src = './pic/tanks_sheet.png';
+
+var flag = false;
+tile.onload = function() {
+    flag = true;
+    imgW = tile.naturalWidth;
+};
+
+var counter = 1;
+
+var tileIndex = 1;
+
+// c.drawImage(var of image, sx, sy, sw, sh,
+//  x, y, width , height)
+
+function render() {
+    var sx = ((tileIndex) % 8)*px;
+    // 1 % 8 *32 => 32
+    // 2 % 8 *32 => 64
+    // 7 % 8 *32 => 224
+    // 8 % 8 *32 => 0
+    // 1 % 8 *32 => 32
+    
+
+    var sy = Math.floor((tileIndex)/8)*px;
+    // 1-7: を0に
+    // 8: の時だけ1に
+    console.log('sx,sy:',sx,sy);
+
+    c.clearRect(0,0,endX,endY);
+        if (flag)
+        c.drawImage(tile, sx,sy,px,px,
+            midX,100+(size),size,size);
+
+        tileIndex++;
+        if (9 == tileIndex)
+            tileIndex = 1;// 0-7 -> 1-8
+
+        requestAnimationFrame(render); // 1000/30fps
+} render();
